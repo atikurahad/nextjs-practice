afc7345e8b103f53c2b53f721036b46893f5310f
unknown <atikurahad@gmail.com>
2024-09-28 17:59:55 +0600

    footer complete

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <div >
-      Welcome to home
+      <Hero/>
 
     </div>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+     <footer className="footer footer-center bg-white text-black font-bold p-4 h-24">
+  <aside>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved by Atikur R. Ahad</p>
+  </aside>
+</footer>
+    </>
+  );
+}
+
+export default Footer;
